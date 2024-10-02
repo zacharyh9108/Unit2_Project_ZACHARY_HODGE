cea6790a7a661e010ba986d94cc84687811b2925
BT_2S14_01 <@email>
2024-10-02 10:56:15 -0400

    I fixed syntax errors and logic within LinearEquation.java. I started to compile in EquationRunner.java

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class EquationRunner {
     public static void main(String[] args) {
+        LinearEquation equation = new LinearEquation(1, 2, 3, 4);
+        int slope = equation.getSlope();
+        int yIntercept = equation.getYIntercept();
+        double distance = equation.distancePoint();
+        System.out.println(slope);
+        System.out.println(yIntercept);
+        System.out.println(distance);
+        System.out.println("Your equation: y = " + slope + "x " + "+ " + yIntercept);
 
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter your first coordinate point: ");
+        String userCoord1 = s.nextLine();
+
+
+        Scanner x = new Scanner(System.in);
+        System.out.print("Enter your second coordinate point: ");
+        String userCoord2 = s.nextLine();
+
+
+        System.out.println("First pair: " + userCoord1.substring(1, 2));
+        System.out.println("Second pair: " + userCoord2.substring(3, 4));
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -5,29 +5,31 @@ public class LinearEquation {
     private int y1;
     private int y2;
 
-    public int Equation(int x1, int x2, int y1, int y2) {
+    public LinearEquation(int x1, int x2, int y1, int y2) {
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
         this.y2 = y2;
-        return x1;
     }
+
 
     public int getSlope() {
         // Return value uses slope formula to calculate slope
         return (y2 - y1) / (x2 - x1);
     }
 
-    public double getYIntercept() {
-        return y1 / (getSlope() * x1);
+    public int getYIntercept() {
+        return y1 - (getSlope() * x1);
     }
 
     public double distancePoint() {
         double a = Math.pow((x2 - x1), 2);
         double b = Math.pow((y2 - y1), 2);
-        double x = a + b;
-        double distance = Math.sqrt(x);
-        return distance;
+        return Math.sqrt(a + b);
+    }
 
+    public double thirdXValue(double x3) {
+        return (x3 * getSlope()) + getYIntercept();
     }
 }
+
