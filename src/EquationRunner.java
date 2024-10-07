d62a7b8d864ae6a5b0b1abe949eac374e20d6c20
BT_2S14_01 <@email>
2024-10-07 11:06:20 -0400

    Converted coordinate points from strings to integers and added more print statements

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,27 +1,39 @@
 import java.util.Scanner;
+import java.lang.String;
 public class EquationRunner {
     public static void main(String[] args) {
-        LinearEquation equation = new LinearEquation(1, 2, 3, 4);
-        int slope = equation.getSlope();
-        int yIntercept = equation.getYIntercept();
-        double distance = equation.distancePoint();
-        System.out.println(slope);
-        System.out.println(yIntercept);
-        System.out.println(distance);
-        System.out.println("Your equation: y = " + slope + "x " + "+ " + yIntercept);
-
 
         Scanner s = new Scanner(System.in);
         System.out.print("Enter your first coordinate point: ");
         String userCoord1 = s.nextLine();
 
 
-        Scanner x = new Scanner(System.in);
+        Scanner w = new Scanner(System.in);
         System.out.print("Enter your second coordinate point: ");
-        String userCoord2 = s.nextLine();
+        String userCoord2 = w.nextLine();
 
+        String stringX1 = userCoord1.substring(userCoord1.indexOf("(") + 1, userCoord1.indexOf(","));
+        String stringY1 = userCoord1.substring(userCoord1.indexOf(",") + 1, userCoord1.indexOf(")"));
+        String stringX2 = userCoord2.substring(userCoord2.indexOf("(") + 1, userCoord2.indexOf(","));
+        String stringY2 = userCoord2.substring(userCoord2.indexOf(",") + 1, userCoord2.indexOf(")"));
 
-        System.out.println("First pair: " + userCoord1.substring(1, 2));
-        System.out.println("Second pair: " + userCoord2.substring(3, 4));
+        int x1 = Integer.parseInt(stringX1);
+        int y1 = Integer.parseInt(stringY1);
+        int x2 = Integer.parseInt(stringX2);
+        int y2 = Integer.parseInt(stringY2);
+
+        LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
+
+        System.out.println("Distance between points: " + equation.distancePoint());
+        System.out.println("Your equation: y = " + equation.getSlope() + "x " + "+ " + equation.getYIntercept());
+        System.out.println("First pair: " + userCoord1);
+        System.out.println("Second pair: " + userCoord2);
+
+        Scanner z = new Scanner(System.in);
+        System.out.print("Enter third x-value: ");
+        String userX3 = z.nextLine();
+        double x3 = Double.parseDouble(userX3);
+
+        System.out.println("Solved third pair: " + "(" + x3 + "," + equation.thirdXValue(x3) + ")");
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,31 +1,30 @@
 import java.lang.Math;
 public class LinearEquation {
     private int x1;
-    private int x2;
     private int y1;
+    private int x2;
     private int y2;
 
-    public LinearEquation(int x1, int x2, int y1, int y2) {
+    public LinearEquation(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
-        this.x2 = x2;
         this.y1 = y1;
+        this.x2 = x2;
         this.y2 = y2;
     }
 
 
-    public int getSlope() {
+    public double getSlope() {
         // Return value uses slope formula to calculate slope
         return (y2 - y1) / (x2 - x1);
     }
 
-    public int getYIntercept() {
+    public double getYIntercept() {
         return y1 - (getSlope() * x1);
     }
 
     public double distancePoint() {
-        double a = Math.pow((x2 - x1), 2);
-        double b = Math.pow((y2 - y1), 2);
-        return Math.sqrt(a + b);
+        double a = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+        return Math.round(a * 100.00) / 100.00;
     }
 
     public double thirdXValue(double x3) {
