1e1577d7e160354fe59f4e1ce55c107a942a8db1
Zacha <@email.com>
2024-10-11 17:09:55 -0400

    Added my new features to design document. Added method in order to make slope a fraction. Made the outputs in my runner file more aesthetically pleasing.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -24,16 +24,30 @@ public class EquationRunner {
 
         LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
 
-        System.out.println("Distance between points: " + equation.distancePoint());
-        System.out.println("Your equation: y = " + equation.getSlope() + "x " + "+ " + equation.getYIntercept());
-        System.out.println("First pair: " + userCoord1);
-        System.out.println("Second pair: " + userCoord2);
+        // Prints the coordinate pairs, slope, y-intercept, distance, and equation in slope-intercept form
+        System.out.println();
+        System.out.println("First pair: " + "(" + x1 + "," + y1 + ")");
+        System.out.println("Second pair: " + "(" + x2 + "," + y2 + ")");
+        System.out.println();
+
+        if (equation.getYIntercept() < 0) {
+            System.out.println("Equation: " + "y = " + equation.getSlope() + "x " + equation.getYIntercept());
+        }
+        else {
+            System.out.println("Equation: " + "y = " + equation.getSlope() + "x" + " + " + equation.getYIntercept());
+        }
+
+        System.out.println();
+        System.out.println("Slope: " + equation.getSlope());
+        System.out.println("Y-Intercept: " + equation.getYIntercept());
+        System.out.println("Distance between points: " + equation.distancePoints());
+        System.out.println();
 
         Scanner z = new Scanner(System.in);
         System.out.print("Enter third x-value: ");
         String userX3 = z.nextLine();
         double x3 = Double.parseDouble(userX3);
 
-        System.out.println("Solved third pair: " + "(" + x3 + "," + equation.thirdXValue(x3) + ")");
+        System.out.println("Solved third pair: " + "(" + x3 + "," + equation.thirdYValue(x3) + ")");
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -12,23 +12,40 @@ public class LinearEquation {
         this.y2 = y2;
     }
 
-
-    public double getSlope() {
-        // Return value uses slope formula to calculate slope
-        return (y2 - y1) / (x2 - x1);
+    // Calculate and return the slope using the slope formula
+    public double slope() {
+        return (double)(y2 - y1)/(x2 - x1);
     }
 
+    // Converts slope into fraction form if needed
+    public String getSlope() {
+        int numerator = (y2 - y1);
+        int denominator = (x2 - x1);
+        if (numerator % denominator == 0) {
+            return String.valueOf(numerator / denominator);
+        }
+        else {
+            return numerator + "/" + denominator;
+        }
+    }
+
+    // Calculate and return the y-intercept based on the slope and coordinates
     public double getYIntercept() {
-        return y1 - (getSlope() * x1);
+        return y1 - (slope() * x1);
     }
 
-    public double distancePoint() {
+    // Calculate and return the distance between two points using the distance formula
+    public double distancePoints() {
         double a = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
         return Math.round(a * 100.00) / 100.00;
     }
 
-    public double thirdXValue(double x3) {
-        return (x3 * getSlope()) + getYIntercept();
+    // Calculate and return the y-coordinate corresponding to x3 based on the slope-intercept form
+    public double thirdYValue(double x3) {
+        return (x3 * slope()) + getYIntercept();
     }
-}
+
+    }
+
+
 
