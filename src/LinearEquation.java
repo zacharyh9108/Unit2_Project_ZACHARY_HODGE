5570d9347251711242077a675c87b9dc34b65200
Zachary <zacharyhodge08@gmail.com>
2025-01-20 03:57:55 -0500

    Update LinearEquation.java
    Y-value is rounded hundredth and getSlope() method displays a more user friendly slope

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,9 +1,9 @@
 import java.lang.Math;
 public class LinearEquation {
-    private int x1;
-    private int y1;
-    private int x2;
-    private int y2;
+    private final int x1;
+    private final int y1;
+    private final int x2;
+    private final int y2;
 
     public LinearEquation(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
@@ -14,7 +14,7 @@ public class LinearEquation {
 
     // Calculate and return the slope using the slope formula
     public double slope() {
-        return (double)(y2 - y1)/(x2 - x1);
+        return (double)(y2 - y1) / (x2 - x1);
     }
 
     // Converts slope into fraction form if needed
@@ -24,28 +24,29 @@ public class LinearEquation {
         if (numerator % denominator == 0) {
             return String.valueOf(numerator / denominator);
         }
-        else {
-            return numerator + "/" + denominator;
+        else if (numerator < 0 && denominator < 0) {
+            numerator = Math.abs(numerator);
+            denominator = Math.abs(denominator);
         }
+        else if (numerator > 0 && denominator < 0) {
+            denominator = Math.abs(denominator);
+        }
+        return numerator + "/" + denominator;
     }
 
     // Calculate and return the y-intercept based on the slope and coordinates
     public double getYIntercept() {
-        return y1 - (slope() * x1);
+        return Math.round((y1 - (slope() * x1)) * 100.0) / 100.0;
     }
 
     // Calculate and return the distance between two points using the distance formula
     public double distancePoints() {
         double a = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
-        return Math.round(a * 100.00) / 100.00;
+        return Math.round(a * 100.0) / 100.0;
     }
 
     // Calculate and return the y-coordinate corresponding to x3 based on the slope-intercept form
     public double thirdYValue(double x3) {
-        return (x3 * slope()) + getYIntercept();
+        return Math.round(((x3 * slope()) + getYIntercept()) * 100.0) / 100.0;
     }
-
-    }
-
-
-
+}
